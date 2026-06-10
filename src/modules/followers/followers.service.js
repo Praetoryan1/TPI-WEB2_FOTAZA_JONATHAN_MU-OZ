11cd88c50da69e28b43bcb5e192a472de0541329
Praetoryan1 <jonathanezequielm20@gmail.com>
2026-06-10 04:00:32 -0300

    Notificacion al seguir (No se envia en unfollow)

--- src/modules/followers/followers.service.js
+++ src/modules/followers/followers.service.js
@@ -1,5 +1,7 @@
 const { Follower, User } = require('../../database/models');
 
+const { createNotification } = require('../notifications/notifications.service');
+
 const toggleFollow = async ({ followerId, followedId }) => {
   if (Number(followerId) === Number(followedId)) {
     throw new Error('No podés seguirte a vos mismo.');
@@ -31,6 +33,15 @@ const toggleFollow = async ({ followerId, followedId }) => {
     follower_id: followerId,
     followed_id: followedId
   });
+
+  await createNotification({
+  userId: followedId,
+  actorId: followerId,
+  type: 'follow',
+  entityType: 'user',
+  entityId: followerId,
+  message: 'Comenzaron a seguirte.'
+});
 
   return {
     following: true,
